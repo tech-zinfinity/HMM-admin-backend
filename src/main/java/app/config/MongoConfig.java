dc3e03434fa822b6b6d9f5a05601d22dfe294379
RahulVarmaa <rahulvarmaa624@gmail.com>
2021-04-11 14:02:57 +0530

    some
    d r

---
+++ src/main/java/app/config/MongoConfig.java
@@ -0,0 +1,10 @@
+package app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableReactiveMongoAuditing;
+
+@Configuration
+@EnableReactiveMongoAuditing
+public class MongoConfig {
+
+}
